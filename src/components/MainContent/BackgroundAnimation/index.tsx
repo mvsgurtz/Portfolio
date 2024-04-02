ea34609607147cf8f38915bbece7136b3a6fb23d
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-04-02 19:30:24 -0300

    Header and intro first ideas and creations.

---
+++ src/components/MainContent/BackgroundAnimation/index.tsx
@@ -0,0 +1,15 @@
+import style from "./style.module.scss";
+
+export const BackgroundAnimation = () => {
+    return (
+        <>
+        <div className={style.background__circle}>
+        
+        </div>
+
+        <div className={style.complementary__circle}>
+        
+        </div>
+        </>
+    );
+}
--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,25 +1,36 @@
+import { BackgroundAnimation } from "./BackgroundAnimation";
+import style from "./style.module.scss";
+
 export const MainContent = () => {
     return (
-        <div>
-            <section>
-                <div>
-                    <h1>NOME</h1>
-                    <h2>Fullstack Developer</h2>
+        <div className={style.main__content}>
+            <section className={style.intro__container}>
+                <div className={style.intro__content}>
+                    <div>
+                        <h1 className={style.name__intro}>Marcos Goulart</h1>
+                        <h2 className={style.stack__intro}>Fullstack Developer</h2>
+                    </div>
+                    <div className={style.portfolio__intro}>
+                        <span>Welcome</span>
+                        <span>to my</span>
+                        <span>Portfolio</span>       
+                    </div>
                 </div>
-                <div>
-                    <h1>Welcome to my portfolio</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores enim fuga quia sapiente dolore animi autem odio? Laboriosam iusto odio voluptates alias obcaecati unde sed?</p>
-                </div>
+                
+                <span className={style.impactPhrase__intro}>“If you think or dream that you can, go for it. Boldness possesses geniality, power and magic. Dare to do it and the power shall be granted.”</span>
+            
             </section>
+            
+            <BackgroundAnimation />
 
-            <section>
+            <section className={style.aboutMe__content}>
                 <div>
                     <h1>About Me</h1>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit aperiam error consectetur minus ut delectus ad laborum odit fugiat. Magnam nihil maiores dignissimos aliquid maxime!</p>
                 </div>
             </section>
 
-            <section>
+            <section className={style.hrdSkills__content}>
                     <h1>Hard Skills</h1>
                     <div>
                         <div>
@@ -29,7 +40,7 @@ export const MainContent = () => {
                     </div>
             </section>
 
-            <section>
+            <section className={style.projects__content}>
                 <div>
                     <h1>Projects</h1>
                     <div>
@@ -49,7 +60,7 @@ export const MainContent = () => {
                 </div>
             </section>
 
-            <section>
+            <section className={style.sftSkills__content}>
                 <h1>Soft Skills</h1>
                 <div>
                     <img src="" alt="" />
